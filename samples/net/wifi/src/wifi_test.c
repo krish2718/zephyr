825aef9a5f9deda4ac5914bc914033a318cb0262
Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>
2024-06-24 22:23:22 +0530

    misc:
    * enable crypto
    * increase sockets
    * Wi-Fi as default Iface
    * tweak stack sizes
    
    Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

--- samples/net/wifi/src/wifi_test.c
+++ samples/net/wifi/src/wifi_test.c
@@ -9,6 +9,19 @@
 
 int main(void)
 {
+#ifdef CONFIG_NET_CONFIG_SETTINGS
+	/* Without this, DHCPv4 starts on first interface and if that is not Wi-Fi or
+	 * only supports IPv6, then its an issue. (E.g., OpenThread)
+	 *
+	 * So, we start DHCPv4 on Wi-Fi interface always, independent of the ordering.
+	 */
+	const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_wifi));
+	struct net_if *wifi_iface = net_if_lookup_by_dev(dev);
 
+	/* As both are Ethernet, we need to set specific interface*/
+	net_if_set_default(wifi_iface);
+
+	net_config_init_app(dev, "Initializing network");
+#endif
 	return 0;
 }
